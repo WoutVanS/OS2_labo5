65d52c8e0edbe4f0227ac6608c2b650503f7e206
kai5555 <51211901+kai5555@users.noreply.github.com>
2022-12-22 09:48:58 +0100

    Update sbuffer.h

--- sbuffer.h
+++ sbuffer.h
@@ -9,7 +9,7 @@
 #endif
 
 #include "config.h"
-
+#include <pthread.h>
 #define SBUFFER_FAILURE -1
 #define SBUFFER_SUCCESS 0
 
@@ -36,8 +36,6 @@ bool sbuffer_is_closed(sbuffer_t* buffer);
         internal to the buffer, users should not
         be concerned with it!
 */
-void sbuffer_lock(sbuffer_t* buffer);
-void sbuffer_unlock(sbuffer_t* buffer);
 
 /**
  * Inserts the sensor data in 'data' at the start of 'buffer' (at the 'head')
@@ -57,3 +55,5 @@ sensor_data_t sbuffer_remove_last(sbuffer_t* buffer);
  * Closes the buffer. This signifies that no more data will be inserted.
  */
 void sbuffer_close(sbuffer_t* buffer);
+
+void mgrs(sbuffer_t* buffer, unsigned long datamgr, unsigned long strgmgr);
